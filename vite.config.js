c12a45e061b57e61055a668e013bad7cb0326aef
Arwy Zam <arwenoras12@gmail.com>
2026-03-10 22:08:53 -0600

    Prueba 6, saquenme de este sufrimiento llamado big pickle

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  build: {
-    assetsDir: "assets",
-  },
   server: {
     proxy: {
       "/api": {
